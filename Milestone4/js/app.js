b1929b9ca7b2b282f48a62f9c054808c3e135649
pannellalino <lista>
2022-11-09 15:02:47 +0100

    mile4

--- Milestone4/js/app.js
+++ Milestone4/js/app.js
@@ -1,10 +1,13 @@
 const {createApp} = Vue;
+const dt = luxon.DateTime;
+const now = dt.now();
 
 createApp({
   data(){
     return{
       newMsgString: '',
       contactActive: 0,
+      searchContact: '',
       contacts:[
         {
         name: 'Michele',
@@ -12,22 +15,22 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '15:30',
+            date: '10/01/2022 15:30:55',
             message: 'Hai portato a spasso il cane?',
             status: 'send'
           },
           {
-            date: '15:30',
+            date: '10/01/2022 15:50:00',
             message: 'Hai steso i panni?',
             status: 'send'
           },
           {
-            date: '16:00',
+            date: '10/01/2022 16:15:22',
             message: 'No!',
             status: 'received'
           },
           {
-            date: '16:01',
+            date: '10/01/2022 16:16:22',
             message: 'Bene, bravo!',
             status: 'send'
           }
@@ -39,17 +42,17 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '15:30',
+            date: '20/03/2022 16:30:00',
             message: 'Ciao come stai?',
             status: 'send'
           },
           {
-            date: '16:00',
+            date: '20/03/2022 16:30:55',
             message: 'Bene grazie, stasera ci vediamo?',
             status: 'received'
           },
           {
-            date: '16:00',
+            date: '20/03/2022 16:35:00',
             message: 'Zero testa ;)',
             status: 'send'
           }
@@ -61,17 +64,17 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '10:30',
+            date: '28/03/2022 10:10:40',
             message: 'La Marianna va in campagna?',
             status: 'send'
           },
           {
-            date: '10:37',
+            date: '28/03/2022 10:20:10',
             message: 'Mi sa che hai sbagliato chat!',
             status: 'received'
           },
           {
-            date: '10:42',
+            date: '28/03/2022 16:15:22',
             message: 'Ah scusa!',
             status: 'send'
           }
@@ -83,12 +86,12 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '15:30',
+            date: '10/01/2022 15:30:55',
             message: 'Lo sai che ha aperto una nuova pizzeria?',
             status: 'send'
           },
           {
-            date: '16:00',
+            date: '10/01/2022 15:50:00',
             message: 'Sì però preferisco il cinema',
             status: 'received'
           }
@@ -100,12 +103,12 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '12:30',
+            date: '10/01/2022 15:30:55',
             message: 'Ricordati di chiamare la nonna',
             status: 'send'
           },
           {
-            date: '12:52',
+            date: '10/01/2022 15:50:00',
             message: 'Sì stasera la sento!',
             status: 'received'
           }
@@ -117,12 +120,12 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '16:30',
+            date: '10/01/2022 15:30:55',
             message: 'Ciao Claudia hai novità?',
             status: 'send'
           },
           {
-            date: '16:45',
+            date: '10/01/2022 15:50:00',
             message: 'No',
             status: 'received'
           }
@@ -134,17 +137,17 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '15:30',
+            date: '10/01/2022 15:30:55',
             message: 'Fai gli auguri a Martina che oggi è il suo compleanno',
             status: 'send'
           },
           {
-            date: '16:00',
+            date: '10/01/2022 15:50:00',
             message: 'Top',
             status: 'received'
           },
           {
-            date: '16:00',
+            date: '10/01/2022 15:50:10',
             message: 'Mi stavo dimenticando, grazie',
             status: 'received'
           }
@@ -156,13 +159,18 @@ createApp({
         visible: true,
         messages:[
           {
-            date: '15:30',
-            message: 'Sai cosa stai facendo su Js',
+            date: '10/01/2022 15:30:55',
+            message: 'Ciao, andiamo a mangiare la pizza stasera?',
             status: 'send'
           },
           {
-            date: '16:00',
-            message: 'Mmm.. Mi sa di no',
+            date: '10/01/2022 15:50:00',
+            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+            status: 'received'
+          },
+          {
+            date: '10/01/2022 15:51:00',
+            message: 'OK!!',
             status: 'received'
           }
          ]
@@ -172,8 +180,9 @@ createApp({
   },
   methods:{
     addMessage(){
+
         const newMessage = {
-        date: '15:00',
+        date: '10/01/2022 18:10:06',
         message: this.newMsgString,
         status: 'send'
       }
@@ -187,14 +196,16 @@ createApp({
     },
     autoRisposta(){
       const newRisposta = {
-        date: '18:00',
+        date: '10/01/2022 18:50:00',
         message: 'Ok!',
         status: 'received'
       }
       this.contacts[this.contactActive].messages.push(newRisposta);
     },
-    changeUser(){
-
+    filteredContact(){
+      if(this.searchContact){
+        return contacts.filter(this.name);
+      }
     }
   }
 }).mount('#app');
